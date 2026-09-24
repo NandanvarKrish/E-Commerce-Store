7f0066e6e24716134c412180851c8e53596ad29b
Krish Nandanvar <155374128+NandanvarKrish@users.noreply.github.com>
2026-09-24 14:21:18 +0530

    Create route.ts

---
+++ src/app/auth/callback/route.ts
@@ -0,0 +1,19 @@
+import { createClient } from "@/utils/supabase/server";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function GET(request: NextRequest) {
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
+  const next = requestUrl.searchParams.get("next") ?? "/account";
+
+  if (code) {
+    const supabase = createClient();
+    const { error } = await supabase.auth.exchangeCodeForSession(code);
+    if (!error) {
+      return NextResponse.redirect(new URL(next, request.url));
+    }
+  }
+
+  // Return to login with error if exchange fails
+  return NextResponse.redirect(new URL("/login?error=auth_callback_failed", request.url));
+}
